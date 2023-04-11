50e0576dd8f1112d74f856c4a7efdb5610dfe99a
KEERTHIKA-01 <126392563+KEERTHIKA-01@users.noreply.github.com>
2023-04-11 22:33:41 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class sample
+{
+ public static void main(String[] input)
+ {
+ System.out.println("Java Tutorial at codescracker.com");
+ }
+}
---
+++ two string .java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+ public static void main(String[] args) {
+ // first string
+ String s1 = "GeeksforGeeks";
+ // second string
+ String s2 = "Geeks for geeks";
+ // check condition
+ if (s1.equals(s2)) {
+ System.out.println("Strings Are Equal");
+ } else {
+ System.out.println("Strings Are Not Equal");
+ }
+ }
+}
